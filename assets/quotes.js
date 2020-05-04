8736c45dd7b09ead34d236d203636c89297a4dfe
onn1900 <64752383+onn1900@users.noreply.github.com>
2020-05-04 12:18:05 +0700

    quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "undefinited00", quotes: "EVERYTHING WAS BEAUTIFUL" },
-    { author: "undefinited00", quotes: "AKU KAMU, dan Dia sebagai Modennnnn #AyoRabi🥰" },
-    { author: "undefinited00", quotes: "Mau Gak jadi Pacarku...????" },
-    { author: "undefinited00", quotes: "I love you." },
-    { author: "undefinited00", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "undefinited00", quotes: "Hari ini, hari esok, Hari-Hari bersamamu" },
-    { author: "undefinited00", quotes: "I'm sorry for loving you😞😞😞." },
+    { author: "~/NOVITA RAMADHANI FIRDAUS/~", quotes: "EVERYTHING WAS BEAUTIFUL" },
+    { author: "~/NOVITA RAMADHANI FIRDAUS/~", quotes: "AKU KAMU, dan Dia sebagai Modennnnn #AyoRabi🥰" },
+    { author: "~/NOVITA RAMADHANI FIRDAUS/~", quotes: "Mau Gak jadi Pacarku...????" },
+    { author: "~/NOVITA RAMADHANI FIRDAUS/~", quotes: "I love you." },
+    { author: "~/NOVITA RAMADHANI FIRDAUS/~", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "~/NOVITA RAMADHANI FIRDAUS/~", quotes: "Hari ini, hari esok, Hari-Hari bersamamu" },
+    { author: "~/NOVITA RAMADHANI FIRDAUS/~", quotes: "I'm sorry for loving you😞😞😞." },
 ];
