0e604866060225d09dfabc10f3c505dfb5f00cda
onn1900 <64752383+onn1900@users.noreply.github.com>
2020-05-04 23:02:41 +0800

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "~/MacheJndr00/~", quotes: "EVERYTHING WAS BEAUTIFUL" },
-    { author: "~/MacheJndr00/~", quotes: "AKU KAMU, dan Dia sebagai Modennnnn #AyoRabi🥰" },
-    { author: "~/MacheJndr00/~", quotes: "Mau Gak jadi Pacarku...????" },
-    { author: "~/MacheJndr00/~", quotes: "Maafkeun kegabutanku dan Kebucinankuu." },
-    { author: "~/MacheJndr00/~", quotes: "parampam pam pam dar jedar." },
-    { author: "~/MacheJndr00/~", quotes: "Hari ini, hari esok, Hari-Hari bersamamu" },
-    { author: "~/MacheJndr00/~", quotes: "harraaaaa karena gabut jadi iseng buat ini untuk kamuuu🤣🤣🤣." },
+    { author: "~/Arulirit00/~", quotes: "EVERYTHING WAS BEAUTIFUL with You " },
+    { author: "~/Arulirit00/~", quotes: "AKU KAMU, dan Dia sebagai Modennnnn #AyoRabi🥰" },
+    { author: "~/Arulirit00/~", quotes: "Corona Jangan kemana mana, abis Corona aku siap CONRONA RONO KARO AWKMU...????" },
+    { author: "~/Arulirit00/~", quotes: "Maafkeun kegabutanku dan Kebucinankuu." },
+    { author: "~/Arulirit00/~", quotes: "parampam pam pam dar jedar." },
+    { author: "~/Arulirit00/~", quotes: "Hari ini, hari esok, Hari-Hari bersamamu" },
+    { author: "~/Arulirit00/~", quotes: "harraaaaa karena gabut jadi iseng buat ini untuk kamuuu🤣🤣🤣." },
 ];
